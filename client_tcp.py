1852a382d1df08eb377030667150f59368eb15a8
Tofef <116598147+Tofef@users.noreply.github.com>
2023-01-27 14:25:03 +0200

    TCP Update
    include an option to multiple messages between the client and the server.

--- client_tcp.py
+++ client_tcp.py
@@ -1,21 +1,38 @@
-# create a socket
-import socket
+# # create a socket
+# import socket
 
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect to the server
+# # Connect to the server
+# IP = "127.0.0.1"    # Local Host
+# PORT = 8820 
+# my_socket.connect((IP, PORT))
+
+# # Send data
+# my_socket.send("hello".encode())
+
+# # Recieve data
+# MAX_MSG_LENGTH = 1024
+# data = my_socket.recv(MAX_MSG_LENGTH).decode()
+# print("The server sent: " + data)
+
+# # Close the socket
+# my_socket.close()
+
 IP = "127.0.0.1"    # Local Host
 PORT = 8820 
+MAX_MSG_LENGTH = 1024
+
+import socket
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 my_socket.connect((IP, PORT))
 
+data = ""
+while (data != "Bye"):
+    msg = input("Please input your message:\n")
+    my_socket.send(msg.encode())
+    data = my_socket.recv(MAX_MSG_LENGTH).decode()
+    print("The server sent: " + data)
 
-# Send data
-my_socket.send("hello".encode())
-
-# Recieve data
-MAX_MSG_LENGTH = 1024
-data = my_socket.recv(MAX_MSG_LENGTH).decode()
-print("The server sent: " + data)
-
-# Close the socket
 my_socket.close()
--- server_tcp.py
+++ server_tcp.py
@@ -1,31 +1,58 @@
-# create a socket
+# # create a socket
+# import socket
+
+# server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# # Assign IP address and port number
+# IP = "0.0.0.0"
+# PORT = 8820
+# server_socket.bind((IP, PORT))
+
+# # Listen
+# server_socket.listen()
+# print("Server is up and running")
+
+# # Connection to the Client
+# (client_socket, client_adress) = server_socket.accept()
+# print("Client connected")
+
+# # Recieve data
+# MAX_MSG_LENGTH = 1024
+# data = client_socket.recv(MAX_MSG_LENGTH).decode()
+# print("Client sent: " + data)
+
+# # Send data
+# client_socket.send(data.encode())
+
+# # Disconnection from the Client
+# client_socket.close()
+
+# # Close the socket
+# server_socket.close()
+
+IP = "0.0.0.0"
+PORT = 8820
+MAX_MSG_LENGTH = 1024
+
 import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Assign IP address and port number
-IP = "0.0.0.0"
-PORT = 8820
 server_socket.bind((IP, PORT))
-
-# Listen
 server_socket.listen()
 print("Server is up and running")
 
-# Connection to the Client
 (client_socket, client_adress) = server_socket.accept()
 print("Client connected")
 
-# Recieve data
-MAX_MSG_LENGTH = 1024
-data = client_socket.recv(MAX_MSG_LENGTH).decode()
-print("Client send: " + data)
+while True:
+    data = client_socket.recv(MAX_MSG_LENGTH).decode()
+    print("Client sent: " + data)
+    if (data == "Quit"):
+        print("closing client socket now...")
+        client_socket.send('Bye'.encode())
+        break
+    client_socket.send(data.encode())
 
-# Send data
-client_socket.send(data.encode())
-
-# Disconnection from the Client
 client_socket.close()
-
-# Close the socket
 server_socket.close()
