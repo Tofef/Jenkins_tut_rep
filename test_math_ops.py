af36121f006e74dfe4a4d69dbb51d0eca5a6127c
Tofef <116598147+Tofef@users.noreply.github.com>
2023-01-31 20:31:01 +0200

    Update test_math_ops.py

--- test_math_ops.py
+++ test_math_ops.py
@@ -1,8 +1,15 @@
 from math_ops import *
 
-def test_add():
-    assert add(2,3) == 5
+print ('-------------------')
+print('')
 
+print ('test add')
+print ('The expected result: 5')
+print ('The function result:' + add(2,3))
 
-def test_sub():
-    assert sub(3,2) == 1
+print ('test sub')
+print ('The expected result: 1')
+print ('The function result:' + sub(3,2))
+
+print('')
+print ('-------------------')
